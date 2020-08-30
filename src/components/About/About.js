c262deaf49719774055d9274b0e343d9d2c984e9
Geno McNew <genemcnew3@gmail.com>
2020-08-30 17:36:00 -0500

    About section with carousel

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,17 @@ import './About.scss';
 
 const About = (props) => {
   return (
-    <div>
-    <img id="headshot" src='https://i.ibb.co/wKFkf8q/758596895.png' alt="Geno McNew" />
+    <div id="" className="text-left">
+       <img id="headshot" className="float-right" src='https://i.ibb.co/wKFkf8q/758596895.png' alt="Geno McNew" />
+      <span id="bioGreeting">Hello! I'm Geno McNew</span>
+      <br />
+      <span id="bioBlurb">
+        I am a life-long learner who enjoys making things that solve problems for people and organizations.
+        I am never satisfied with "this is how we do it, and how we've been doing it for years."
+        I have always tried my best to improve on things, and this has continued into my experience in software development.
+        I make scripts and programs to improve the quality of my work life and I'm good at finding things that need to be fixed.
+        Let Me learn how to work with you and your team
+      </span>
     </div>
   );
 };
---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+
+const Carousel = (props) => {
+  const items = [];
+  let key = 0;
+  props.projects.forEach((project) => {
+    key += 1;
+    const item = {
+      src: project.imgUrl,
+      altText: project.name,
+      caption: project.name,
+      header: project.name,
+      key,
+    };
+    items.push(item);
+  });
+  return <UncontrolledCarousel items={items} />;
+};
+export default Carousel;
